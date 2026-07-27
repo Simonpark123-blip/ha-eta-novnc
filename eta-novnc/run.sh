d8f920fcc4b2fab94d684bb4b786538807f0238b
Tobias Hufnagel <tobias.hufnagel@msg.group>
2026-07-27 22:02:38 +0200

    Added ingress

--- eta-novnc/run.sh
+++ eta-novnc/run.sh
@@ -2,29 +2,11 @@
 
 VNC_HOST=$(bashio::config 'vnc_host')
 VNC_PORT=$(bashio::config 'vnc_port')
-VNC_PASSWORD=$(bashio::config 'vnc_password')
 
 bashio::log.info "Starte ETA noVNC"
 bashio::log.info "VNC Ziel: ${VNC_HOST}:${VNC_PORT}"
 
-if [ -n "$VNC_PASSWORD" ]; then
-    bashio::log.info "VNC-Passwort ist gesetzt"
-
-    echo "$VNC_PASSWORD" > /tmp/vnc_password
-    chmod 600 /tmp/vnc_password
-
-    /opt/websockify/run \
-        --web /opt/noVNC \
-        --websockify-token-plugin TokenFile \
-        6080 \
-        "${VNC_HOST}:${VNC_PORT}" &
-else
-    bashio::log.info "Kein VNC-Passwort konfiguriert"
-
-    /opt/websockify/run \
-        --web /opt/noVNC \
-        6080 \
-        "${VNC_HOST}:${VNC_PORT}" &
-fi
-
-wait
+exec /opt/websockify/run \
+    --web /opt/noVNC \
+    6080 \
+    "${VNC_HOST}:${VNC_PORT}"
